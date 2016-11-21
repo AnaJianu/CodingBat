d2b8083994c2fd283f5c477b4b1c1501775bac74
Ana Jianu <ana.jianu@yahoo.com>
2016-11-21 17:30:10 +0200

    Given a string, return a string where for every char in the original, there are two chars.

---
+++ String2/src/com/codingbat/DoubleChar.java
@@ -0,0 +1,17 @@
+package com.codingbat;
+
+/**
+ * Given a string, return a string where for every char in the original, there are two chars.
+ */
+public class DoubleChar {
+
+    public String doubleChar(String str) {
+        String result="";
+
+        for (int i=0; i<str.length(); i++) {
+            result=result+str.substring(i,i+1)+str.substring(i,i+1);
+        }
+
+        return result;
+    }
+}
