fec1412e7e5ccf2eac8b69d2d602b5e286a3da24
Ana Jianu <ana.jianu@yahoo.com>
2016-11-21 16:52:27 +0200

    Given two arrays of ints sorted in increasing order, outer and inner, return true if all of the numbers in inner appear in outer. The best solution makes only a single "linear" pass of both arrays, taking advantage of the fact that both arrays are already in sorted order.

--- Array3/src/com/codingbat/CanBalance.java
+++ Array3/src/com/codingbat/CanBalance.java
@@ -1,7 +1,8 @@
 package com.codingbat;
 
 /**
- * Created by ana on 21/11/2016.
+ * Given a non-empty array, return true if there is a place to split the array so that the
+ * sum of the numbers on one side is equal to the sum of the numbers on the other side.
  */
 public class CanBalance {
     public boolean canBalance(int[] nums) {
