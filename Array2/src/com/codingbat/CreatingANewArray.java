09bbf7e34c25a7135a857b2e06aac1c9e48961a5
Ana Jianu <ana.jianu@yahoo.com>
2016-11-22 16:05:47 +0200

    Given a number n, create and return a new int array of length n, containing the numbers 0, 1, 2, ... n-1. The given n may be 0, in which case just return a length 0 array. You do not need a separate if-statement for the length-0 case; the for-loop should naturally execute 0 times in that case, so it just works.

---
+++ Array2/src/com/codingbat/CreatingANewArray.java
@@ -0,0 +1,20 @@
+package com.codingbat;
+
+/**
+ * Given a number n, create and return a new int array of length n, containing the numbers 0, 1, 2, ... n-1.
+ * The given n may be 0, in which case just return a length 0 array. You do not need a separate if-statement
+ * for the length-0 case; the for-loop should naturally execute 0 times in that case, so it just works.
+ */
+public class CreatingANewArray {
+
+    public int[] fizzArray(int n) {
+
+        int[] newArray=new int[n];
+
+        for (int i=0; i<n; i++) {
+            newArray[i]=i;
+        }
+
+        return newArray;
+    }
+}
