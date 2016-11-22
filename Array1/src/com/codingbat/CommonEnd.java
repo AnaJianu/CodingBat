6c84c1863312c1522ff0c31f250e83e8227da332
Ana Jianu <ana.jianu@yahoo.com>
2016-11-22 16:00:48 +0200

    Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more.

---
+++ Array1/src/com/codingbat/CommonEnd.java
@@ -0,0 +1,13 @@
+package com.codingbat;
+
+/**
+ * Given 2 arrays of ints, a and b, return true if they have the same first element
+ * or they have the same last element. Both arrays will be length 1 or more.
+ */
+public class CommonEnd {
+
+    public boolean commonEnd(int[] a, int[] b) {
+
+        return (a[0]==b[0] || (a[a.length-1]==b[b.length-1]));
+    }
+}
