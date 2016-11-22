7b6d926d2027feba603b260ed254999bd9f61962
Ana Jianu <ana.jianu@yahoo.com>
2016-11-22 16:03:56 +0200

    Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.

---
+++ Array1/src/com/codingbat/MiddleWay.java
@@ -0,0 +1,15 @@
+package com.codingbat;
+
+/**
+ * Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+ */
+public class MiddleWay {
+
+    public int[] middleWay(int[] a, int[] b) {
+
+        int[] newArray={a[1], b[1]};
+
+
+        return newArray;
+    }
+}
